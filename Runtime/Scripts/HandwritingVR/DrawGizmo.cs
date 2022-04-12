1757afd25bc177f40d8fa1ddd550115c094f1833
Kapilas Gnanarajah <kapilas.gnanarajah@stud.unibas.ch>
2022-04-12 15:56:25 +0200

    Optimized Gizmo for better debugging

--- Runtime/Scripts/HandwritingVR/DrawGizmo.cs
+++ Runtime/Scripts/HandwritingVR/DrawGizmo.cs
@@ -10,7 +10,7 @@ namespace HandwritingVR
         private DrawingData _collectData;
         private List<Vector3> _boundingBox;
         private List<Vector2> _bound2D;
-        private List<List<Vector2>> _normProjPoints;
+        private List<List<Vector2>> _proj2D;
 
         public void SetCollectData(DrawingData data)
         {
@@ -22,7 +22,7 @@ namespace HandwritingVR
         {
             if (_collectData)
             {
-                Gizmos.DrawSphere(_collectData.GetSupportVector(), 0.01f);
+                /*Gizmos.DrawSphere(_collectData.GetSupportVector(), 0.01f);
                 // Gizmos.DrawLine(_collectData.GetSupportVector(), _collectData.GetNormalVector());
                 // Gizmos.DrawLine(new Vector3(0.0f, 0.0f, 0.0f), _collectData.GetNormalVector());
                 // Gizmos.color = Color.yellow;
@@ -43,17 +43,7 @@ namespace HandwritingVR
                 // Gizmos.color = Color.yellow;
                 // Gizmos.DrawLine(new Vector3(0.0f, 0.0f, 0.0f), _collectData.GetSupportVector());
                 //Gizmos.DrawLine(new Vector3(0.0f, 0.0f, 0.0f), _collectData.GetSupportVector().normalized);
-
-                Gizmos.color = Color.cyan;
-                var projectedSegments = _collectData.GetProjectedSegments();
-                foreach (var segment in projectedSegments)
-                {
-                    for (int i = 0; i < segment.Count-1; i++)
-                    {
-                        Gizmos.DrawLine(segment[i], segment[i+1]);
-                    }
-                }
-
+                
                 if (_boundingBox == null || _bound2D == null)
                 {
                     _boundingBox = _collectData.GetBoundingBox();
@@ -78,7 +68,8 @@ namespace HandwritingVR
                     Gizmos.DrawLine(_boundingBox[3], _boundingBox[0]);
                     Gizmos.DrawLine(_bound2D[3], _bound2D[0]);
                 }
-                /*for (int i = 0; i < boundingBox.Count-1; i++)
+                
+                for (int i = 0; i < boundingBox.Count-1; i++)
                 {
                     Gizmos.DrawLine(boundingBox[i], boundingBox[i+1]);
                     if (i == boundingBox.Count - 2)
@@ -88,18 +79,13 @@ namespace HandwritingVR
                 }*/
 				
                 Gizmos.color = Color.cyan;
-                if (_normProjPoints == null)
+                _proj2D = _collectData.Get2DSegments();
+                if (_proj2D is null) return;
+                foreach (var segment in _proj2D)
                 {
-                    _normProjPoints = _collectData.GetNormalizedSegments();
-                }
-                else
-                {
-                    foreach (var segment in _normProjPoints)
+                    for (int i = 0; i < segment.Count-1; i++)
                     {
-                        for (int i = 0; i < segment.Count-1; i++)
-                        {
-                            Gizmos.DrawLine(segment[i],segment[i+1]);
-                        }
+                        Gizmos.DrawLine(segment[i],segment[i+1]);
                     }
                 }
             }
