dc4bdb5e3ae14e0c18e921c552b8d833c72ad43e
Kapilas Gnanarajah <kapilas.gnanarajah@stud.unibas.ch>
2022-04-29 17:14:15 +0200

    optimized fuzzy value calculations for segments

--- Runtime/Scripts/HandwritingVR/Segment.cs
+++ Runtime/Scripts/HandwritingVR/Segment.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
 using UnityEngine;
 
 namespace HandwritingVR
@@ -12,6 +13,7 @@ namespace HandwritingVR
         private List<Vector2> _points;
         private List<Vector2> _charBoundBox;
         private List<Vector2> _segBoundBox;
+        private float _segmentLength;
 
         // Fuzzy values of segment
         // HP Horizontal Position
@@ -34,9 +36,10 @@ namespace HandwritingVR
 
         // SY Center Y
         public String SY;
-
-        /*public String EX;
-        public String EY;*/
+        
+        // Is a Dot like on i
+        public Boolean isDot;
+        
         // MSTR Straightness
         public String MSTR;
 
@@ -54,9 +57,7 @@ namespace HandwritingVR
 
         // Negative slant
         public String NS;
-
-        /*public String VC;
-        public String HC;*/
+        
         // AL A-Like
         public String AL;
 
@@ -82,24 +83,28 @@ namespace HandwritingVR
             SetFuzzyValues();
         }
 
+        private void SetLength()
+        {
+            var l = 0f;
+            for (int i = 0; i < _points.Count-1; i++)
+            {
+                l += Vector2.Distance(_points[i], _points[i + 1]);
+            }
+            _segmentLength = l;
+        }
         private void SetFuzzyValues()
         {
             SetHP();
             SetVP();
+            SetSX();
+            SetSY();
+            IsDot();
             SetHLEN();
             SetVLEN();
             SetSLEN();
-            SetSX();
-            SetSY();
             SetMSTRandMARC();
-            SetVL();
-            SetHL();
-            SetPS();
-            SetNS();
-            SetAL();
-            SetDL();
-            SetCL();
-            SetUL();
+            AngleCalculations();
+            ArcCalculations();
             SetOL();
         }
 
@@ -157,38 +162,55 @@ namespace HandwritingVR
         private void SetHP()
         {
             float xCenter = (_segBoundBox[0].x + _segBoundBox[1].x) / 2;
-            var hp = (xCenter - _segBoundBox[0].x) / (_segBoundBox[1].x - _segBoundBox[0].x);
+            var hp = (xCenter - _charBoundBox[0].x) / (_charBoundBox[1].x - _charBoundBox[0].x);
+            // Debug.Log("relativ Horizontal position: "+hp);
             HP = MembershipFunction(hp);
         }
 
         private void SetVP()
         {
             float yCenter = (_segBoundBox[1].y + _segBoundBox[2].y) / 2;
-            var vp = (yCenter - _segBoundBox[1].y) / (_segBoundBox[2].y - _segBoundBox[1].y);
+            var vp = (yCenter - _charBoundBox[1].y) / (_charBoundBox[2].y - _charBoundBox[1].y);
+            // Debug.Log("relativ vertical position: "+vp);
             VP = MembershipFunction(vp);
         }
 
         private void SetHLEN()
         {
-            float d = (_points[0] - _points[^1]).magnitude;
+            float d = _segBoundBox[1].x - _segBoundBox[0].x; // Vector2.Distance(_points[0], _points[^1]);
             float width = _charBoundBox[1].x - _charBoundBox[0].x;
             var hlen = d / width;
+            if (hlen > 1)
+            {
+                hlen = 1f;
+            }
+            // Debug.Log("HLEN value: "+hlen);
             HLEN = MembershipFunction(hlen);
         }
 
         private void SetVLEN()
         {
-            float d = (_points[0] - _points[^1]).magnitude;
+            float d = _segBoundBox[2].y - _segBoundBox[1].y; // Vector2.Distance(_points[0], _points[^1]);
             float height = _charBoundBox[2].y - _charBoundBox[1].y;
             var vlen = d / height;
+            if (vlen > 1)
+            {
+                vlen = 1f;
+            }
+            // Debug.Log("VLEN value: "+vlen);
             VLEN = MembershipFunction(vlen);
         }
 
         private void SetSLEN()
         {
-            float d = (_points[0] - _points[^1]).magnitude;
-            float slant = (_charBoundBox[0] - _charBoundBox[2]).magnitude;
+            float d = Vector2.Distance(_segBoundBox[0], _segBoundBox[2]); // Vector2.Distance(_points[0], _points[^1]);
+            float slant = Vector2.Distance(_charBoundBox[0], _charBoundBox[2]);
             var slen = d / slant;
+            if (slen > 1)
+            {
+                slen = 1f;
+            }
+            // Debug.Log("SLEN value: "+slen);
             SLEN = MembershipFunction(slen);
         }
 
@@ -206,12 +228,11 @@ namespace HandwritingVR
 
         private void SetMSTRandMARC()
         {
-            // TODO verify correctness
-            float d = (_points[0] - _points[^1]).magnitude;
+            float d = Vector2.Distance(_points[0], _points[^1]);
             float sum = 0.0f;
             for (int i = 0; i < _points.Count - 1; i++)
             {
-                float dl = (_points[i] - _points[i + 1]).magnitude;
+                float dl = Vector2.Distance(_points[i], _points[i + 1]);
                 sum += dl;
             }
 
@@ -219,6 +240,8 @@ namespace HandwritingVR
             var marc = 1 - mstr;
             MSTR = MembershipFunction(mstr);
             MARC = MembershipFunction(marc);
+            // Debug.Log("SegmentNumber "+segmentNumber+", Straightness: "+MSTR);
+            // Debug.Log("SegmentNumber "+segmentNumber+", Arcness: "+MARC);
         }
 
         private float Delta(float x, float b, float c)
@@ -228,13 +251,13 @@ namespace HandwritingVR
             var i2 = c + (b / 2);
             if (i1 <= x && x <= i2)
             {
-                res = 1 - 2 * Math.Abs((x - c) / 2);
+                res = 1 - 2 * Math.Abs((x - c) / b);
             }
 
             return res;
         }
-
-        private void SetVL()
+        
+        private void AngleCalculations()
         {
             Vector2 x = new Vector2(1, 0);
             Vector2 a = _points[^1] - _points[0];
@@ -243,120 +266,125 @@ namespace HandwritingVR
             {
                 angle += 360;
             }
-
+            
             var vl = Math.Max(Delta(angle, 90, 90), Delta(angle, 90, 270));
             VL = MembershipFunction(vl);
-        }
-
-        private void SetHL()
-        {
-            Vector2 x = new Vector2(1, 0);
-            Vector2 a = _points[^1] - _points[0];
-            var angle = Vector2.SignedAngle(x, a);
-            if (angle < 0)
-            {
-                angle += 360;
-            }
-
             var hl = Math.Max(Delta(angle, 90, 0),
                 Math.Max(Delta(angle, 90, 180), Delta(angle, 90, 360)));
             HL = MembershipFunction(hl);
-        }
-
-        private void SetPS()
-        {
-            Vector2 x = new Vector2(1, 0);
-            Vector2 a = _points[^1] - _points[0];
-            var angle = Vector2.SignedAngle(x, a);
-            if (angle < 0)
-            {
-                angle += 360;
-            }
-
             var ps = Math.Max(Delta(angle, 90, 45), Delta(angle, 90, 225));
             PS = MembershipFunction(ps);
-        }
-
-        private void SetNS()
-        {
-            Vector2 x = new Vector2(1, 0);
-            Vector2 a = _points[^1] - _points[0];
-            var angle = Vector2.SignedAngle(x, a);
-            if (angle < 0)
-            {
-                angle += 360;
-            }
-
             var ns = Math.Max(Delta(angle, 90, 135), Delta(angle, 90, 315));
             NS = MembershipFunction(ns);
         }
 
-        private void SetAL()
+        private void ArcCalculations()
         {
+            var xs = _segBoundBox[0].x;
+            var xe = _segBoundBox[1].x;
             var ys = _segBoundBox[1].y;
             var ye = _segBoundBox[2].y;
-            var asum = 0;
+            var aSum = 0f;
+            var uSum = 0f;
+            var dSum = 0f;
+            var cSum = 0f;
+            
             foreach (var point in _points)
             {
                 if (point.y > (ys + ye) / 2)
                 {
-                    asum += 1;
+                    aSum += 1;
                 }
-            }
-
-            var al = Math.Min(1, asum / _points.Count);
-            AL = MembershipFunction(al);
-        }
-
-        private void SetDL()
-        {
-            var xs = _segBoundBox[0].x;
-            var xe = _segBoundBox[1].x;
-            var dsum = 0;
-            foreach (var point in _points)
-            {
-                if (point.x > (xs + xe) / 2)
-                {
-                    dsum += 1;
-                }
-            }
-
-            var dl = Math.Min(1, dsum / _points.Count);
-            DL = MembershipFunction(dl);
-        }
-
-        private void SetCL()
-        {
-            var xs = _segBoundBox[0].x;
-            var xe = _segBoundBox[1].x;
-            var csum = 0;
-            foreach (var point in _points)
-            {
-                if (point.x < (xs + xe) / 2)
-                {
-                    csum += 1;
-                }
-            }
-
-            var cl = Math.Min(1, csum / _points.Count);
-            CL = MembershipFunction(cl);
-        }
-
-        private void SetUL()
-        {
-            var ys = _segBoundBox[1].y;
-            var ye = _segBoundBox[2].y;
-            var usum = 0;
-            foreach (var point in _points)
-            {
                 if (point.y < (ys + ye) / 2)
                 {
-                    usum += 1;
+                    uSum += 1;
+                }
+                if (point.x > (xs + xe) / 2)
+                {
+                    dSum += 1;
+                }
+                if (point.x < (xs + xe) / 2)
+                {
+                    cSum += 1;
                 }
             }
-
-            var al = Math.Min(1, usum / _points.Count);
+            var al = Math.Min(1f, aSum / _points.Count);
+            var ul = Math.Min(1f, uSum / _points.Count);
+            var dl = Math.Min(1f, dSum / _points.Count);
+            var cl = Math.Min(1f, cSum / _points.Count);
+            CL = MembershipFunction(cl);
+            DL = MembershipFunction(dl);
+            UL = MembershipFunction(ul);
             AL = MembershipFunction(al);
+            
+            /*var aLen = 0f;
+            var uLen = 0f;
+            var dLen = 0f;
+            var cLen = 0f;
+            for (int i = 0; i < _points.Count-1; i++)
+            {
+                var p0 = _points[i];
+                var p1 = _points[i + 1];
+                if (p0.y > (ys + ye) / 2)
+                {
+                    if (p1.y > (ys + ye) / 2)
+                    {
+                        aLen += Vector2.Distance(p0, p1);
+                    }
+                    else
+                    {
+                        var x = ((p1.x - p0.x) / (p1.y - p0.y)) * (ys + ye) / 2;
+                        aLen += Vector2.Distance(p0, new Vector2(x, (ys + ye) / 2));
+                    }
+                }
+                
+                if (p0.y < (ys + ye) / 2)
+                {
+                    if (p1.y < (ys + ye) / 2)
+                    {
+                        uLen += Vector2.Distance(p0, p1);
+                    }
+                    else
+                    {
+                        var x = ((p1.x - p0.x) / (p1.y - p0.y)) * (ys + ye) / 2;
+                        uLen += Vector2.Distance(p0, new Vector2(x, (ys + ye) / 2));
+                    }
+                }
+                if (p0.x > (xs + xe) / 2)
+                {
+                    if (p1.x > (xs + xe) / 2)
+                    {
+                        dLen += Vector2.Distance(p0, p1);
+                    }
+                    else
+                    {
+                        var y = ((p1.x - p0.x) / (p1.y - p0.y)) * (xs + xe) / 2;
+                        dLen += Vector2.Distance(p0, new Vector2((xs + xe) / 2,y));
+                    }
+                }
+                if (p0.x < (xs + xe) / 2 && p1.x < (xs + xe) / 2)
+                {
+                    if (p1.x < (xs + xe) / 2)
+                    {
+                        cLen += Vector2.Distance(p0, p1);
+                    }
+                    else
+                    {
+                        var y = ((p1.x - p0.x) / (p1.y - p0.y)) * (xs + xe) / 2;
+                        cLen += Vector2.Distance(p0, new Vector2((xs + xe) / 2,y));
+                    }
+                    
+                }
+                
+            }
+            */
+            /*
+            Debug.Log("number of points: "+_points.Count);
+            Debug.Log("C-Like: "+CL+", value: "+cl+" with sum: "+cSum); //+",  length: "+ cLen_segmentLength);
+            Debug.Log("D-Like: "+DL+", value: "+dl+" with sum: "+dSum); //+",  length: "+ dLen/_segmentLength);
+            Debug.Log("U-Like: "+UL+", value: "+ul+" with sum: "+uSum); //+",  length: "+ uLen_segmentLength);
+            Debug.Log("A-Like: "+AL+", value: "+al+" with sum: "+aSum); //+",  length: "+ aLen_segmentLength);
+            */
         }
 
         private void SetOL()
@@ -364,9 +392,11 @@ namespace HandwritingVR
             float xCenter = (_segBoundBox[0].x + _segBoundBox[1].x) / 2;
             float yCenter = (_segBoundBox[1].y + _segBoundBox[2].y) / 2;
             Vector2 center = new Vector2(xCenter, yCenter);
+            // compare radius
             float rExp = ((_segBoundBox[1].x - _segBoundBox[0].x) + (_segBoundBox[2].y - _segBoundBox[1].y)) / 4;
             float rAct = 0;
-            var pExp = 2 * Math.PI * rExp;
+            // compare perimeter
+            var pExp = 2 * Math.PI * rExp; 
             float pAct = 0;
             for (int i = 0; i < _points.Count - 1; i++)
             {
@@ -376,7 +406,7 @@ namespace HandwritingVR
             }
 
             rAct += (_points[^1] - center).magnitude;
-            rAct = rAct / _points.Count;
+            rAct /= _points.Count;
             float f = (float) (pAct / pExp);
             var g = rAct / rExp;
             var ol1 = 0f;
@@ -401,13 +431,25 @@ namespace HandwritingVR
 
             var ol = Math.Min(ol1, ol2);
             OL = MembershipFunction(ol);
+            // Debug.Log("O-like: "+OL+", with perimeter ol1: "+ol1+", with radius ol2: "+ol2);
         }
 
-        // TODO Hockey stick "Jj"(right/left)"l", Walking stick "f"(right/left)"1"
+        private void IsDot()
+        {
+            if (_points.Count <= 3)
+            {
+                isDot = true;
+            }
+            else
+            {
+                isDot = false;
+            }
+        }
+        // Hockey stick "Jj"(right/left)"l", Walking stick "f"(right/left)"1"
 
         private string MembershipFunction(float f)
         {
-            if (0<= f && f <= 0.0835f)
+            if (0 <= f && f <= 0.0835f)
             {
                 return "VS"; // Very Small
             }
@@ -416,22 +458,27 @@ namespace HandwritingVR
             {
                 return "S"; // Small
             }
+
             if (0.2505f < f && f <= 0.4175f)
             {
                 return "SM"; // Small Medium
             }
+
             if (0.4175f < f && f <= 0.5845f)
             {
                 return "M"; // Medium
             }
+
             if (0.5845f < f && f <= 0.7515f)
             {
                 return "LM"; // Large Medium
             }
+
             if (0.7515f < f && f <= 0.9165f)
             {
                 return "L"; // Large
             }
+
             if (0.9165f < f && f <= 1f)
             {
                 return "VL"; // Very Large
